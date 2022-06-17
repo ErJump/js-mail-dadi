da882295929736ed42d5ec6c24272798c59ce4fe
ErJump <fedelijunior@gmail.com>
2022-06-17 16:56:55 +0200

    Adds For Loop for checking email

--- js/main.js
+++ js/main.js
@@ -3,7 +3,17 @@ const userMail = prompt('inserisci la email (registrate: giampiero.fedeli@gmail.
 //genero un array con varie email
 const mailList = ['giampiero.fedeli@gmail.com' , 'torofurio@gmail.com', 'amogliarray@gmail.com', 'ginobino@gmail.com' , 'noisia@info.com' , 'signoremio@gmail.com' , 'lessgo@gmail.com'];
 //creo la condizione per la quale l'email inserita dall'utente deve essere inclusa all'interno della lista per iniziare il gioco, altrimenti viene mostrato l'errore
-if (mailList.includes(userMail)){
+
+let mailCheck = false; 
+
+for (let i = 0; i < mailList.length ; i++){
+    const element = mailList[i];
+    if (element === userMail){
+        mailCheck = true;
+    }
+}
+
+if (mailCheck === true){
     alert('Email registrata - Inizia il gioco')
     //dichiaro le costanti per il giocatore e l'ai
     const userNumber =  Math.floor((Math.random() * 6) + 1);
